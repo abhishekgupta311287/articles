f93501c29c853aa038298f5a0942d2d8709b0964
abhishekviu <abhishek.gupta@vuclip.com>
2020-08-08 18:44:17 +0530

    Added main application module

---
+++ app/src/main/java/com/abhishekgupta/articles/ArticlesActivity.kt
@@ -0,0 +1,12 @@
+package com.abhishekgupta.articles
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ArticlesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
